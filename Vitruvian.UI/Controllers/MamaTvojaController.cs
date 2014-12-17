445ad3e924ebc3e3c2a2c46de3642106da14fd2a
Atila Bala <atila.bala@devtechgroup.com>
2014-12-17 09:34:48 +0100

    Base commit

---
+++ Vitruvian.UI/Controllers/MamaTvojaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Vitruvian.UI.Controllers
+{
+    public class MamaTvojaController : Controller
+    {
+        // GET: MamaTvoja
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Vitruvian.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Vitruvian.UI.MvcApplication" Language="C#" %>
